ac1d870f6d03a01e487f625c8a923ec2d754682e
shiyanshirani <shiyan99s@gmail.com>
2021-10-17 16:38:32 +0530

    added assets and configured README

--- OckyPocky/OckyPocky/settings.py
+++ OckyPocky/OckyPocky/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f4_%6&stz&evji1%c6p280wy%$6)w@dqme84(rf7)*@qa^2!s0"
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
